f98bea5b58f348716dc55b8ba696b273182250ee
ARYASUTA22 <aryasuta.susantio@gmail.com>
2025-10-01 15:35:16 +0700

    Commit 2: Favorites and Search Bar

---
+++ data.js
@@ -0,0 +1,65 @@
+const cultureData = [
+    {
+        id: 'kudus',
+        imgSrc: 'masjidMenaraKudus.jpg',
+        imgAlt: 'Masjid Menara Kudus',
+        title: 'Masjid Menara Kudus',
+        description: 'Simbol arsitektur Islam di Indonesia yang berpadu dengan kearifan lokal Hindu-Buddha.'
+    },
+    {
+        id: 'keroncong',
+        imgSrc: 'musikKeroncong.jpg',
+        imgAlt: 'Musik Keroncong',
+        title: 'Musik Keroncong',
+        description: 'Alunan merdu perpaduan harmonis antara musik Portugis dan melodi khas Melayu Nusantara.'
+    },
+    {
+        id: 'wayang',
+        imgSrc: 'wayangKulit.jpg',
+        imgAlt: 'Wayang Kulit',
+        title: 'Wayang Kulit',
+        description: 'Kisah epik dari India yang diceritakan melalui bayangan khas Jawa, kaya akan filosofi hidup.'
+    },
+    {
+        id: 'kuliner',
+        imgSrc: 'peranakan.jpg',
+        imgAlt: 'Kuliner Peranakan',
+        title: 'Kuliner Peranakan',
+        description: 'Cita rasa Tionghoa yang berpadu dengan kekayaan rempah Nusantara di dapur Indonesia.'
+    },
+    {
+        id: 'barongsai',
+        imgSrc: 'barongsai.jpg',
+        imgAlt: 'Kesenian Barongsai',
+        title: 'Kesenian Barongsai',
+        description: 'Tarian singa dari Tiongkok yang menari lincah di tanah Garuda, menjadi simbol kebersamaan.'
+    },
+    {
+        id: 'blenduk',
+        imgSrc: 'gereja.jpg',
+        imgAlt: 'Gereja Blenduk',
+        title: 'Gereja Blenduk',
+        description: 'Pesona arsitektur Neoklasik Eropa dengan kubah khasnya yang menyatu dengan sejarah Kota Lama Semarang.'
+    },
+    {
+        id: 'gambang',
+        imgSrc: 'gamelan.jpg',
+        imgAlt: 'Gambang Kromong',
+        title: 'Gambang Kromong',
+        description: 'Harmoni ceria yang lahir dari perpaduan alat musik Tionghoa dengan gamelan khas Betawi.'
+    },
+    {
+        id: 'lasem',
+        imgSrc: 'batikLasem.jpg',
+        imgAlt: 'Batik Lasem',
+        title: 'Batik Lasem',
+        description: 'Kain batik pesisir Jawa yang dihiasi motif naga dan warna merah cerah khas Tionghoa.'
+    },
+    {
+        id: 'tabuik',
+        imgSrc: 'tabuik.jpg',
+        imgAlt: 'Tradisi Tabuik',
+        title: 'Tradisi Tabuik',
+        description: 'Festival megah peringatan Asyura di Pariaman, hasil perpaduan tradisi Islam Syiah dan adat Minang.'
+    }
+];
